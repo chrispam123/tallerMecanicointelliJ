6442c6055170a6c2d07482e2329e74c9f3c4f065
chrispam123 <uopechris@gmail.com>
2026-02-11 07:20:36 +0100

    feat:Añadida la logica de carpetas

--- Taller.java
+++ Taller.java
@@ -1,16 +1,63 @@
+import java.io.File;
 import java.util.ArrayList; //inicializamos el arraylist para guardar datos
+import java.util.Scanner;
+
 public class Taller {
 
 
     private ArrayList<Vehiculo> inventario; //definimos declaramos el arrylist modelo vacio
+    //definimos las rutas constanstes
+    private static final String CARPETA = "data";
+    private static final String ARCHIVO = "data/vehiculos.txt";
+    //variables en mayúsculas por convención
 
     //constructor
     public Taller() {
 
         this.inventario = new ArrayList<>(); //dejamos espacio en memoria
+        //llamamos a la preparación apenas se cree el taller
+        prepararArchivo();
 
     }
-    //metodo agregar vehiculo
+    //preparar carpetas y archivos es lo primero
+    private void prepararArchivo(){
+        try{
+            //revisamos la carpeta
+            File directorio = new File(CARPETA);
+            if(!directorio.exists()){
+                if(directorio.mkdir()){
+                    System.out.println("Carpeta iniciado exitosamente");
+
+                }
+            }
+            else {
+                System.out.println("Ya existe un Carpeta data");
+            }
+
+            //revisamos el archivo
+            File archivo = new File(ARCHIVO);
+            if(!archivo.exists()){
+                if(archivo.createNewFile()) {
+
+                    System.out.println("Achivos de vehiculos creados");
+
+                }
+
+
+            }
+            else {
+                System.out.println("Ya existe un archivos vehiculos.txt");
+            }
+
+        }catch(Exception e){
+            System.out.println("error al crear el archivo");
+            
+        }
+
+
+    }
+
+    //metodo agregar vehiculo este es el inventario
     public boolean agregarVehiculo(String placa, String marca, String modelo ) {
 
         Vehiculo coche = new Vehiculo(placa, marca, modelo);
@@ -23,14 +70,6 @@ public class Taller {
             System.out.println(coche);
         }
     }
-
-
-
-
-
-
-
-
 
 
 
